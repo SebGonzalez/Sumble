c33b377b1c342a53e2d727cbf5dae93877c9b03b
Gabriel Curinga <gabriel.curinga@etu.unice.fr>
2017-03-21 15:32:21 +0100

    Layout Fin activity

--- java/fr/unice/iutnice/sumble/View/FinActivity.java
+++ java/fr/unice/iutnice/sumble/View/FinActivity.java
@@ -2,8 +2,10 @@ package fr.unice.iutnice.sumble.View;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Parcelable;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityCompat.OnRequestPermissionsResultCallback;
@@ -12,6 +14,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import fr.unice.iutnice.sumble.Model.Connexion.SendScore;
 import fr.unice.iutnice.sumble.Model.Score;
@@ -19,12 +24,27 @@ import fr.unice.iutnice.sumble.R;
 
 public class FinActivity extends AppCompatActivity {
 
+    private Button retourMenu;
+    private TextView scoreFin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fin);
 
         Score score = getIntent().getExtras().getParcelable("score");
+
+        retourMenu = (Button)findViewById(R.id.retourMenu);
+        retourMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FinActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        scoreFin = (TextView)findViewById(R.id.scoreFin);
+        scoreFin.setText(score.getValeur()+"");
 
         SendScore sendScore = new SendScore(this);
         String id = getIntent().getStringExtra("id");
